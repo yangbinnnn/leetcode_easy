59abdfb2fc6f06037cef3cbb8f6ef1e653eaa112
yangbinnnn <yangbinnnn@gmail.com>
2018-01-31 22:05:31 +0800

    better way

--- Linked_List/remove_nth_node_from_end.py
+++ Linked_List/remove_nth_node_from_end.py
@@ -22,6 +22,11 @@ Try to do this in one pass.
 #         self.next = None
 
 class Solution(object):
+    """
+    遍历一次,记录每个节点位置
+    找到第n个节点，将n-1指向n+1
+    注意只有一个节点和删除头节点的情况
+    """
     def removeNthFromEnd(self, head, n):
         """
         :type head: ListNode
@@ -52,3 +57,30 @@ class Solution(object):
         # 只有一个node
         else:
             return None
+
+class Solution1(object):
+    """
+    使用两个游标, p1和p2
+    两个游标相差n个节点, 当p2到达末尾时，p1为next为需要删除的节点
+    
+    """
+    def removeNthFromEnd(self, head, n):
+        """
+        :type head: ListNode
+        :type n: int
+        :rtype: ListNode
+        """
+        p1 = p2 = head
+        # p2移动n
+        for _ in range(n):
+            p2 = p2.next
+        # 删除head
+        if not p2:
+            return p1.next
+        # 开始同步一样,直到最后一个节点
+        while p2.next:
+            p2 = p2.next
+            p1 = p1.next
+        # p1.next 为倒数第n个
+        p1.next = p1.next.next
+        return head
