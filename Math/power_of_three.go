3f0b002a2ef3fb751bb7714201a262abe3a479ed
yangbinnnn <yangbinnnn@gmail.com>
2018-03-20 22:02:21 +0800

    add power of three

---
+++ Math/power_of_three.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+// Given an integer, write a function to determine if it is a power of three.
+//
+// Follow up:
+// Could you do it without using any loop / recursion?
+// 判断一个数是不是3的x次方
+
+func isPowerOfThree(n int) bool {
+	if n < 1 {
+		return false
+	}
+	for n%3 == 0 {
+		n = n / 3
+	}
+	return n == 1
+}
